f6417acc24e008e7501c60442cbe2451bfaa5389
Edarcega <edimar.eds@gmail.com>
2022-03-05 12:27:58 -0300

    DepartmentService

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,8 +15,11 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
+
+	private DepartmentService service;
 
 	// Referêncas para os quatro componentes: Botão table view e as duas colunas da
 	// tabela
@@ -31,9 +37,15 @@ public class DepartmentListController implements Initializable {
 	@FXML
 	private Button btNew;
 
+	private ObservableList<Department> obsList;
+
 	@FXML
 	public void onBtNewAction() {
 		System.out.println("OnBtNewAction");
+	}
+
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 
 	@Override
@@ -49,6 +61,23 @@ public class DepartmentListController implements Initializable {
 		// Ajuste para a tablea acompanhar a altura da janela principal (stage)
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+	}
+
+	// Metodo responsavel por acessar os serviços carregar os departameentos e
+	// repassar para o observable list.
+	// o Observable list é associado ao table view e os departamentos são
+	// apresentados na tela
+	public void updateTableView() {
+		if (service == null) {
+			throw new IllegalStateException("o Service estava nulo");
+		}
+
+		List<Department> list = service.findAll();
+
+		obsList = FXCollections.observableArrayList(list);
+		
+		tableViewDepartment.setItems(obsList);
+
 	}
 
 }
